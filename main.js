185ba1d56eb61ce40523acf849a69f0467147598
RanaTahon <111710924+RanaTahon@users.noreply.github.com>
2024-03-06 14:09:50 +0000

    add checkout page and cart

--- main.js
+++ main.js
@@ -20,3 +20,66 @@ function addProduct(product) {
     `
     shopContent.appendChild(productBox)
 }
+
+
+
+
+let cartArr = [];
+
+function addItem(item) {
+    let productIndex = cartArr.findIndex(function (product) {
+        return product.name == item.name
+    })
+    if (productIndex == -1) {
+        cartArr.push(item)
+    }
+    else {
+        cartArr[productIndex].quantity += item.quantity
+    }
+}
+
+function removeItem(name) {
+    let productIndex = cartArr.findIndex(function (product) {
+        return product.name == name
+    })
+    if (productIndex == -1) {
+        return;
+    }
+    if (cartArr[productIndex].quantity == 1) {
+        cartArr.splice(productIndex, 1)
+    }
+    else {
+        cartArr[productIndex].quantity -= 1
+    }
+}
+
+
+function printReceipt() {
+    console.log("Receipt:");
+    let total = 0;
+    cartArr.forEach(function (product) {
+        let itemTotal = product.quantity * product.price
+        console.log(product.name + " X " + product.quantity + " = " + itemTotal);
+        total += itemTotal
+    })
+
+    console.log("----------------------------------");
+    console.log("Total:" + total);
+}
+
+
+
+
+
+// addItem({ name: "Milk", price: 5, quantity: 1 })
+// addItem({ name: "Milk", price: 5, quantity: 1 })
+// addItem({ name: "Yogurt", price: 10, quantity: 1 })
+// addItem({ name: "Yogurt", price: 10, quantity: 1 })
+// addItem({ name: "Yogurt", price: 10, quantity: 1 })
+
+// printReceipt();
+
+// removeItem('Milk');
+// removeItem('Yogurt');
+
+// printReceipt();
